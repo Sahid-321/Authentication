81ef50af38e6492181b80809b869d6fbf1a6329a
sahid jamal <sahidjamal151@gmail.com>
2022-12-17 21:50:04 +0530

    axios added

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const express = require("express");
 const app = express();
 const PORT = 8080;
-
+const axios = require("axios")
 app.get("/",(req,res)=>{
     res.send("server working")
+    axios.get(`https://jsonplaceholder.typicode.com/todos/`)
+    .then((result) => console.log(result)
+        
+    ).catch((err) => {
+        console.log(err);
+    });
 })
 
 app.listen(PORT,()=>{
