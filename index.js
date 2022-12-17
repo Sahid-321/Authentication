b79536f3217cfd2be68ec89c722bfc933c3d2207
sahid jamal <sahidjamal151@gmail.com>
2022-12-17 21:41:59 +0530

    get api added

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const express = require("express");
 const app = express();
 const PORT = 8080;
 
+app.get("/",(req,res)=>{
+    res.send("server working")
+})
+
 app.listen(PORT,()=>{
     console.log(`server runnign at ${PORT}`);
 })
