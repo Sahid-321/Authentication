619bfaddd0a4bea7ec854747fb764da5dec589fe
sahid jamal <sahidjamal151@gmail.com>
2022-12-18 20:18:11 +0530

    axios working

--- index.js
+++ index.js
@@ -4,12 +4,18 @@ const PORT = 8080;
 const axios = require("axios")
 app.get("/",(req,res)=>{
     res.send("server working")
-    axios.get(`https://jsonplaceholder.typicode.com/todos/`)
+    axios.get(`https://jsonplaceholder.typicode.com/todos/`, { 
+        headers: { "Accept-Encoding": "gzip,deflate,compress" }} )
     .then((result) => console.log(result)
         
     ).catch((err) => {
         console.log(err);
     });
+})
+
+app.post("/",(req,res)=>{
+    req.body.name,
+    red.body.email
 })
 
 app.listen(PORT,()=>{
